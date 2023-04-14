fcdfd9a8b757c09a5a2993a7d9516f316a4838c8
Stepanenko1993 <109295820+Stepanenko1993@users.noreply.github.com>
2023-04-14 23:11:21 +0300

    color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,36 @@
 
-console.log('hello')
+
+
+const start = document.querySelector('button[data-start]');
+const stop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+   
+
+let idInt = null;
+
+stop.disabled = true;
+start.addEventListener('click', startColor);
+stop.addEventListener('click', stopColor);
+
+
+function startColor() {
+    start.disabled = true;
+    stop.disabled = false;
+   
+
+    idInt = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+}
+
+function stopColor() {
+    start.disabled = false;
+    stop.disabled = true;
+ 
+
+    clearInterval(idInt);
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
